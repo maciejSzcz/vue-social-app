38065a0961e5800d86a164478129446d166ec57a
Maciej Szczęsny <szczesnymcj@gmail.com>
2022-01-23 13:41:13 +0100

    init client app

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-loading-bar-provider>
+    <n-message-provider>
+      <n-notification-provider>
+        <n-dialog-provider>
+          <router-view />
+        </n-dialog-provider>
+      </n-notification-provider>
+    </n-message-provider>
+  </n-loading-bar-provider>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="primary">Primary</n-button>
+      <n-button type="info">Info</n-button>
+      <n-button type="success">Success</n-button>
+      <n-button type="warning">Warning</n-button>
+      <n-button type="error">Error</n-button>
+    </n-space>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
